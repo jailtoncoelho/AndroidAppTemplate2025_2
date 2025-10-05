9c93d33b578fbff8bffb86b3ba86f3890cf8a7a3
Ericson Schmidt Bicalho <schcodes@gmail.com>
2025-10-05 15:06:40 -0300

    Revert "salva novas informações de usuário no realtime database"
    This reverts commit 3d42527fe0e5aa5b0858b341ab7b6cff4d7bc0c9.

--- app/src/main/java/com/ifpr/androidapptemplate/baseclasses/Usuario.kt
+++
@@ -1,9 +0,0 @@
-package com.ifpr.androidapptemplate.baseclasses
-
-
-data class Usuario(
-    var key: String? = null,
-    var nome: String? = null,
-    var email: String? = null,
-    var endereco: String? = null
-)
